bf02c79e89604af19cf89102c2002b7cb895006c
Rafael Rain <rafaelrainbr@outlook.com>
2024-06-24 14:26:09 -0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,33 @@
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+fun main() {
+    embeddedServer(
+        factory = Netty,
+        port = 8080,
+        module = Application::module
+    ).start(wait = true)
+}
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+
+    routing {
+        get("/") {
+            call.respond(Response("Hello World"))
+        }
+    }
+}
+
+@Serializable
+data class Response(
+    val message: String
+)
